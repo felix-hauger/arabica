69164c78457255a6ccb10a5659cc38b2d6a5dc2b
lucas-ribard <lucas.ribard@laplateforme.io>
2024-03-14 13:06:37 +0100

    fixed WriteStringForCompile and added writing logic for arg1

--- src/main.c
+++ src/main.c
@@ -17,13 +17,14 @@ int main(int argc, char **argv)
     int filedes = open("bytecode", O_CREAT | O_RDWR);
 
     // char *processName = getName(argc, argv); // get the name of the process
-    writeHeader(filedes, 24, programName);
+    writeHeader(filedes, 18, programName);
 
+    /*
     char load_val = getFunctionCodeFromName("LOAD_VAL");
     char add = getFunctionCodeFromName("ADD");
     char print_val = getFunctionCodeFromName("PRINT_VAL");
 
-    parse_abc("test.abc");
+    
 
     write(filedes, &load_val, 1);
     writeIntegerForCompile(filedes, 42);
@@ -31,6 +32,7 @@ int main(int argc, char **argv)
     writeIntegerForCompile(filedes, 69);
     write(filedes, &add, 1);
     write(filedes, &print_val, 1);
+    */
 
     //missing the total number of bytes in the file
 
@@ -41,5 +43,40 @@ int main(int argc, char **argv)
     // char buffer[11] = {0}; // Initialize buffer with 11 zero bytes
     // write(1, buffer, 11);   // Write 11 bytes of zero to stdout
 
+    //get instruction from file
+    _Instruction* result = parse_abc("test.abc");
+
+    for (int i = 0; result[i].instruction != NULL; i++) {
+        _Instruction* instruction = &result[i];
+        
+        //write to file the code for the instruction
+        write(filedes, &instruction->code, 1);
+
+        // if the args are declared ( not "No arg" ) write them to the file
+        if (strcmp(instruction->arguments[0], "No arg") != 0) {
+            if (isdigit(instruction->arguments[0][1])) {  //check if it is a integer
+                printf("arg 1 is an integer\n");
+                writeIntegerForCompile(filedes, atoi(instruction->arguments[0])); 
+                printf("arg 1 printed\n");
+               
+            } else {
+                printf("arg 1 is a string\n");
+                printf("arg1 = %s\n", instruction->arguments[0]);
+                writeStringForCompile(filedes, instruction->arguments[0]);//else its an string
+               
+                printf("arg 1 printed\n");
+               
+            }
+            //if arg 2 is declared write it to the file
+            if (strcmp(instruction->arguments[1], "No arg") != 0) {
+                if (isdigit(instruction->arguments[1])) {
+                     writeIntegerForCompile(filedes, atoi(instruction->arguments[1]));
+                } else {
+                    writeStringForCompile(filedes, instruction->arguments[1]);
+                }
+            }
+        }
+    }
+    
     return 0;
 }
